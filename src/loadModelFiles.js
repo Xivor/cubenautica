fd807264a944f49dca588693f8e7df2995584cb8
Diogo <diogoribfaria@gmail.com>
2025-06-19 00:20:21 -0300

    Refactor asset loading

--- src/loadModelFiles.js
+++
@@ -1,6 +0,0 @@
-function loadModelFile(file) { 
-	let newScript = document.createElement("script"); 
-	newScript.src = file; 
-	document.body.appendChild(newScript);
-}
-loadModelFile("models/untitled.js")
