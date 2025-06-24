520a2d415d068d83fdf1b38faa10ae90262c90b4
Francisco Henriques <franciscolealhenriques@usp.br>
2025-06-23 22:39:07 -0300

    Add toon shader
    The toon shader is just the fragment, using the basic vertex shader as
    base.

--- src/auxiliary.js
+++ src/auxiliary.js
@@ -1,12 +1,16 @@
 window.onresize = function() {
     gCanvas.width = window.innerWidth;
     gCanvas.height = window.innerHeight;
-    
+
     gl.viewport(0, 0, gCanvas.width, gCanvas.height);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     let aspect = window.innerWidth / window.innerHeight;
-    gl.uniformMatrix4fv(gShader.uPerspective, false, flatten(perspective(CAMERA_FOVY, aspect, CAMERA_NEAR, CAMERA_FAR)));
+
+    for (let shader in gShaders) {
+        gl.useProgram(gShaders[shader].program);
+        gl.uniformMatrix4fv(gShaders[shader].uPerspective, false, flatten(perspective(CAMERA_FOVY, aspect, CAMERA_NEAR, CAMERA_FAR)));
+    }
 }
 
 function startFpsDisplay() {
